2c6410e9e3991e85ef564c2218e7493784343116
Eric Taylor <erictaylor4954@gmail.com>
2025-05-31 20:01:15 -0700

    added no ads modifier

--- append_search.js
+++ append_search.js
@@ -1,6 +1,8 @@
 window.onload = function() {
+    const no_aio = " -ai";
+    const no_ads = " -ads";
     document.getElementById('theForm').onsubmit = function() {
         var txt = document.getElementById('txtSearch');
-        txt.value = txt.value + " -ai";
+        txt.value = txt.value + no_aio + no_ads;
     };
 };
